7c80d358b57b4aecf5c864a392c0290e2d789936
UzitheI <ujjwalpuri221@gmail.com>
2025-03-27 20:38:00 +0545

    selection linear

---
+++ lab3/selection_linear.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int partition(int arr[], int l, int r)
+{
+    int pivot = arr[r];
+    int i = l - 1;
+
+    for (int j = l; j < r; j++)
+    {
+        if (arr[j] <= pivot)
+        {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[r]);
+    return i + 1;
+}
+
+int randomPartition(int arr[], int l, int r)
+{
+    srand(time(NULL));
+    int n = r - l + 1;
+    int pivot = rand() % n;
+    swap(&arr[l + pivot], &arr[r]);
+    return partition(arr, l, r);
+}
+
+int kthSmallest(int arr[], int l, int r, int k)
+{
+    if (k > 0 && k <= r - l + 1)
+    {
+        int pos = randomPartition(arr, l, r);
+
+        if (pos - l == k - 1)
+            return arr[pos];
+
+        if (pos - l > k - 1)
+            return kthSmallest(arr, l, pos - 1, k);
+
+        return kthSmallest(arr, pos + 1, r, k - pos + l - 1);
+    }
+    return -1;
+}
+
+int main()
+{
+    int arr[] = {12, 3, 5, 7, 4, 19, 26};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int k = 3;
+
+    printf("Original array: ");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+
+    printf("%dth smallest element is %d\n", k, kthSmallest(arr, 0, n - 1, k));
+
+    return 0;
+}
